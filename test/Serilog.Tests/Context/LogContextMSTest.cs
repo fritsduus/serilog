fba44e8aa3df43efecb2ee61c957d18c1516c39f
Frits Duus Jørgensen <frits.duus@gmail.com>
2014-04-28 10:04:31 +0200

    MSTest that illustrates PushProperty problem

---
+++ test/Serilog.Tests/Context/LogContextMSTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Serilog.Context;
+using Serilog.Events;
+using Serilog.Tests.Support;
+
+namespace Serilog.Tests.Context
+{
+    [TestClass]
+    public class LogContextMSTest
+    {
+        [TestMethod]
+        public void MSTest_MoreNestedPropertiesOverrideLessNestedOnes()
+        {
+            LogEvent lastEvent = null;
+
+            var log = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Sink(new DelegatingSink(e => lastEvent = e))
+                .CreateLogger();
+
+            using (LogContext.PushProperty("A", 1))
+            {
+                log.Write(Some.InformationEvent());
+                Assert.AreEqual(1, lastEvent.Properties["A"].LiteralValue());
+
+                using (LogContext.PushProperty("A", 2))
+                {
+                    log.Write(Some.InformationEvent());
+                    Assert.AreEqual(2, lastEvent.Properties["A"].LiteralValue());
+                }
+
+                log.Write(Some.InformationEvent());
+                Assert.AreEqual(1, lastEvent.Properties["A"].LiteralValue());
+            }
+
+            log.Write(Some.InformationEvent());
+            Assert.IsFalse(lastEvent.Properties.ContainsKey("A"));
+        }
+    }
+}
